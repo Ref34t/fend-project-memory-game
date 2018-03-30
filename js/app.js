f7ac739edf2ad1ee5113c3bc97b1f60e929331fb
Mohamed Refaat <mohammedref34t@gmail.com>
2018-03-30 09:40:15 +0200

    list to hold all the cards

--- js/app.js
+++ js/app.js
@@ -1,29 +1,31 @@
-/*
- * Create a list that holds all of your cards
- */
 
+var card = document.getElementsByClassName('card');
+var newgame = document.querySelector('.restart');
+/*Create a list that holds all of your cards */
+ var cards = Array.from(card);
+ /* Display the cards on the page */
+ /*   - shuffle the list of cards using the provided "shuffle" method  */
+ function shuffle(Array) {
+     var currentIndex = Array.length, temporaryValue, randomIndex;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+     while (currentIndex !== 0) {
+         randomIndex = Math.floor(Math.random() * currentIndex);
+         currentIndex -= 1;
+         temporaryValue = Array[currentIndex];
+         Array[currentIndex] = Array[randomIndex];
+         Array[randomIndex] = temporaryValue;
+     }
+
+     return Array;
+ }
+ newgame.addEventListener('click', shuffle(cards));
+ console.log(cards);
+
+ /*   - loop through each card and create its HTML*/
+ /*   - add each card's HTML to the page*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 
 /*
